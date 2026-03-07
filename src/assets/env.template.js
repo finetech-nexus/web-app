c873ea60753d381b7fd07f9445e5e039b5a4a70e
amiladi <amiladi@yopmail.com>
2026-03-07 16:33:30 +0100

    fix $MIFOS_OAUTH_SERVER_ENABLED env var

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -62,7 +62,7 @@
   window['env']['sessionIdleTimeout'] = '$MIFOS_SESSION_IDLE_TIMEOUT';
 
   // OAuth Server Enabled
-  window['env']['oauthServerEnabled'] = '$MIFOS_OAUTH_SERVER_ENABLED';
+  window['env']['oauthServerEnabled'] = '$MIFOS_OAUTH_SERVER_ENABLED' === 'true';
 
   // OAuth Server URL
   window['env']['oauthServerUrl'] = '$MIFOS_OAUTH_SERVER_URL';
